308e8c95f32d63f12c3ae3cdec3fa523a16cae02
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-03-15 09:01:24 +0800

    fix: Update personal description in Banner component for clarity and accuracy

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -59,7 +59,7 @@ export const Banner = () => {
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1 style={{height: '80px'}} className="header-title">{`Hi! I'm David, `} <span className="txt-rotate" dataPeriod="1000" data-rotate='["Web Developer", "AI enthusiast" ]'><span className="wrap">{text}</span></span></h1>
                   <p> 
-                    I am a highly motivated and aspiring Software and AI Engineer, with a deep passion for technology and innovation. At 19 years old, I'm currently embarking on my academic journey as a second-year Computer Engineering student at the Polytechnic University of the Philippines - Manila. My enthusiasm for software development and artificial intelligence drives me to constantly seek new knowledge and skills, as I aim to contribute meaningfully to the tech industry in the future.              
+                  I am a highly motivated Software and aspiring AI Engineer, with a deep passion for technology and innovation. At 20 years old, my enthusiasm for software development, cloud technologies and artificial intelligence drives me to constantly seek new knowledge and skills, aiming to contribute meaningfully to the tech industry in the future
                   </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
