964ad9c6f9519f491a174b2c41b285c2eb6e1ef6
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-08 01:20:06 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,117 +19,123 @@ import projImg16 from "../../assets/img/proj16.png";
 import projImg17 from "../../assets/img/proj17.png";
 import projImg18 from "../../assets/img/proj18.png";
 import projImg19 from "../../assets/img/proj19.png";
+import projImg20 from "../../assets/img/proj20.png";
 import './Projects.css';
 
 export const Projects = () => {
 
   const projects = [
-    
     {
-      title: "We Chat Application (Chat Application)",
-      description: "Built using MERN stack, uses Socket.io for real-time chat functionality",
+      title: "We Chat Application AI (Chat Application)",
+      description: "MERN stack app with real-time chat and sentiment analysis AI for chat messages.",
       imgUrl: projImg1,
-      link: "https://github.com/DavidBatoDev/chat-application-mongodb"
+      link: "https://github.com/DavidBatoDev/chat-application-mongodb. ",
     },
     {
       title: "PUP Alumni Portal For Graduates (PUPGS)",
-      description: "Built using React as a Frontend, Uses PHP Laravel, MySql for Database, Websocket for real-time notification",
+      description: "React frontend, Laravel backend, MySQL, and WebSocket notifications. For PUP Alumni Graduates.",
       imgUrl: projImg7,
       link: "https://github.com/DavidBatoDev/pup_alumni_portal"
     },
     {
       title: "Arduino Day PH 2025 Website",
-      description: "Built using NextJS for client and server side rendering and uses tailwindcss and shadcn for styling and animations",
+      description: "Official ArduionoPH 2025 Website built using NextJS site with TailwindCSS and animations.",
       imgUrl: projImg16,
       link: "https://github.com/DavidBatoDev/arduino-ph-2025"
     },
     {
+      title: "IskoChatAI",
+      description: "NextJS Typescript chatbot with Gemini API LLM for Philippines scholarship oppurtunities.",
+      imgUrl: projImg20,
+      link: "https://github.com/DavidBatoDev/iskochatai"
+    },
+    {
       title: "Alertech Smart Fire Alert System (Mobile App)",
-      description: "Developed a Smart Fire Alert System mobile app using React Native, Firebase FCM, Firestore, and ESP32 Arduino for real-time fire detection and alerts.",
+      description: "React Native app with Firebase backend and FCM, and ESP32 for fire alerts.",
       imgUrl: projImg19,
       link: "https://github.com/DavidBatoDev/alertech-mobile-app"
     },
     {
       title: "Alertech Smart Fire Alert System (Website)",
-      description: "Created a companion web app for Fire Authorithy for the Smart Fire Alert System using React and Firebase Firestore, providing real-time monitoring alongside the mobile app and IoT integration",
+      description: "React web app for real-time fire monitoring with IoT.",
       imgUrl: projImg18,
       link: "https://github.com/geraldsberongoy/Arduino-Hackathon-Web"
     },
     {
       title: "ElectrifAI Solutions PH Website",
-      description: "Contributing to the developement of the company website using react typescript",
+      description: "React TypeScript company Info website.",
       imgUrl: projImg6,
       link: "https://electrifai.tech/"
     },
     {
       title: "ElectrifAI Solutions PH Mobile App",
-      description: "Contributing to the developement of the company main mobile application using react-native and supabase",
+      description: "React Native app with Supabase integration to manage electricity user details and electricity consumption.",
       imgUrl: projImg8,
       link: ""
     },
     {
       title: "GDG XParky Points API",
-      description: "Built using Flask as Backend, uses Google API for classroom and sheets integration",
+      description: "Flask API with Google Classroom and Sheets integration to track 200+ students participation and experience points.",
       imgUrl: projImg9,
       link: "https://github.com/DavidBatoDev/gdg-web-development-classroom-api"
     },
     {
       title: "Real-estate Website (DavidEstate)",
-      description: "Built using React as a Frontend, Express as a Backend, MongoDB for Database, JWT for Authentication, Redux for State Management",
+      description: "React, Express, MongoDB, JWT, and Redux for managing a mock up real estate website.",
       imgUrl: projImg2,
       link: "https://github.com/DavidBatoDev/real-estate-mongodb"
     },
     {
       title: "Blog Website (Technoquatro)",
-      description: "Built using React as a Frontend, Uses Firebase Storage, Database and Athentication",
+      description: "React app with Firebase for storage and authentication for the Technoquatro students.",
       imgUrl: projImg3,
       link: "https://github.com/DavidBatoDev/technoquatro-firebase"
     },
     {
       title: "E-commerce Website (Ecom)",
-      description: "Built using MySQL, Express, React, Node.js, polishing my skills in state management",
+      description: "MySQL, Express, React, and Node.js for a mock up e-commerce website.",
       imgUrl: projImg4,
       link: "https://github.com/DavidBatoDev/ecommerce-mysql"
     },
     {
-      title: "Student Curicullar Activity Manager (SCAM)",
-      description: "Contributing to build it using Tkinter as a Frontend, Uses Flask Blueprint Structure, SQLAlchemy for Database, Websocket for real-time notes",
+      title: "Student Curricular Activity Manager (SCAM)",
+      description: "Tkinter, Flask, and SQLAlchemy for real-time notes. An all in 1 productivity and collaboration app.",
       imgUrl: projImg5,
       link: "https://github.com/DavidBatoDev/oop-scam-app"
     },
     {
       title: "Email Spam Classifier Pytorch",
-      description: "Built using Pytorch, uses MLP for classification, uses Flask for API",
+      description: "MLP-based spam classifier built from scratch with Flask API. To detect email spams.",
       imgUrl: projImg10,
       link: "https://github.com/DavidBatoDev/email_spam_classifier_pytorch"
     },
     {
       title: "California Housing Price Prediction EDA",
-      description: "Built using Python, uses Pandas, Numpy, Matplotlib, Seaborn for EDA",
+      description: "Python EDA with Pandas, NumPy, and Matplotlib for california housing datasets.",
       imgUrl: projImg12,
       link: "https://github.com/DavidBatoDev/house-price-prediction-pytorch"
     },
     {
       title: "Object Detection (OpenCV)",
-      description: "Built an OpenCV and leverage an existing model for object detection",
+      description: "Object detection using OpenCV and pretrained models that detects 100+ objects real time.",
       imgUrl: projImg11,
       link: "https://github.com/DavidBatoDev/object-detection-opencv"
     },
     {
       title: "Data Structure and Algorithm Visualizer",
-      description: "Built using React as a Frontend, uses framer for animation for visualizing data structures and algorithms",
+      description: "React app with Framer animations for basic Data Strcuture and algorithm visualization.",
       imgUrl: projImg13,
       link: "https://github.com/DavidBatoDev/data-structure-algo-visualization"
     },
     {
       title: "Data Structure and Algorithm Visualizer (Mario Theme)",
-      description: "Built using React as a Frontend, uses framer for animation for visualizing data structures and algorithms",
+      description: "Mario-themed React app with Framer animations for basic Data Strcuture and algorithm visualization.",
       imgUrl: projImg14,
       link: "https://github.com/DavidBatoDev/data-structure-algo-mario-theme"
     },
     {
       title: "Constellation Orb ESP32 Firebase",
-      description: "Uses ESP32 Arduino for IoT, Firebase Real time Database for Backend",
+      description: "ESP32 IoT with Firebase real-time database that connects two orbs even from afar. Perfect for long distance couples.",
       imgUrl: projImg15,
       link: "https://github.com/DavidBatoDev/orblink-esp32-firebase"
     },
