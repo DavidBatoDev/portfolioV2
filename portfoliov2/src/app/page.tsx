7c3753003b61fb911d18ddb2c1c88a224261fc58
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-12 02:23:34 +0800

    fix: update navigation order in Header component and comment out Contact component in main page

--- portfoliov2/src/app/page.tsx
+++ portfoliov2/src/app/page.tsx
@@ -15,7 +15,7 @@ const Index = () => {
       <About />
       <Skills />
       <Projects />
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </div>
   );
--- portfoliov2/src/components/Header.tsx
+++ portfoliov2/src/components/Header.tsx
@@ -31,14 +31,11 @@ const Header = () => {
           <a href="#about" className="nav-link">
             <span className="nav-link-number">01.</span>About
           </a>
-          <a href="#projects" className="nav-link">
-            <span className="nav-link-number">02.</span>Projects
-          </a>
           <a href="#skills" className="nav-link">
-            <span className="nav-link-number">03.</span>Skills
+            <span className="nav-link-number">02.</span>Skills
           </a>
-          <a href="#contact" className="nav-link">
-            <span className="nav-link-number">04.</span>Contact
+          <a href="#projects" className="nav-link">
+            <span className="nav-link-number">03.</span>Projects
           </a>
           <a 
             href="/resume.pdf" 
