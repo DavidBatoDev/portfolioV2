dbb98dec701151c460d31fb99b04f4804a745082
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-12 03:46:30 +0800

    feat: add Chatbot component and integrate Google Generative AI; update dependencies in package.json and package-lock.json

--- portfoliov2/src/app/page.tsx
+++ portfoliov2/src/app/page.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
+import ChatBot from "@/components/Chatbot/Chatbot";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -17,6 +18,7 @@ const Index = () => {
       <Projects />
       {/* <Contact /> */}
       <Footer />
+      <ChatBot />
     </div>
   );
 };
