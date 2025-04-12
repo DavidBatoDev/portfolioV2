fde1e6ae7d186afe08c051628737facd034353fa
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-04-12 18:06:49 +0800

    refactor: comment out unused Contact component import in page.tsx

--- portfoliov2/src/app/page.tsx
+++ portfoliov2/src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Contact from "@/components/Contact";
+// import Contact from "@/components/Contact";
 import ChatBot from "@/components/Chatbot/Chatbot";
 import Footer from "@/components/Footer";
 
